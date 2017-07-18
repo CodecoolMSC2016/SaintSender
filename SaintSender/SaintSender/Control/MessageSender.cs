f26e66d6de97289547010a18838c00c0eaa576c8
Imre Meszesan <imre.meszesan@gmail.com>
2017-07-18 14:58:32 +0200

    implemented MessageSender class

---
+++ SaintSender/SaintSender/Control/MessageSender.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MimeKit;
+using MailKit.Net.Smtp;
+using MailKit;
+
+namespace SaintSender.Control
+{
+    class MessageSender : ISender
+    {
+        private SmtpClient client;
+
+        public MessageSender(SmtpClient client)
+        {
+            this.client = client;
+        }
+
+        public void SendMail(MimeMessage message)
+        {
+            if (!client.IsConnected)
+                throw new ServiceNotConnectedException("Not connected to SMTP server.");
+            if (!client.IsAuthenticated)
+                throw new ServiceNotAuthenticatedException("Client not authorized.");
+
+            client.Send(message);
+        }       
+    }
+}
