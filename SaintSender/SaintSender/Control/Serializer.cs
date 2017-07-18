8bdc2436a5a7faf68096d543b03033f2bd51b4ec
bozsob <bozso.beatrix@gmail.com>
2017-07-18 12:16:17 +0200

    implement Save(Mail[] mails, string folderName) using BinaryFormatter for serialization

---
+++ SaintSender/SaintSender/Control/Serializer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace SaintSender.Control
+{
+    internal class Serializer : ISerializer
+    {
+        public Mail[] Restore(string folderName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save(Mail[] mails, string folderName)
+        {
+            string fileName = folderName + @"\mails.dat";
+            FileStream stream = new FileStream(fileName, FileMode.Create);
+
+            BinaryFormatter bf = new BinaryFormatter();
+
+            for (int i = 0; i < mails.Length; i++)
+            {
+                bf.Serialize(stream, mails[i]);
+            }
+            stream.Close();
+        }
+    }
+}
