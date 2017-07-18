27dfd6fafc7269d79da30846c01b00c799f76a2c
bozsob <bozso.beatrix@gmail.com>
2017-07-18 15:17:49 +0200

    using type MimeMessage instead of type Mail

--- SaintSender/SaintSender/Control/Serializer.cs
+++ SaintSender/SaintSender/Control/Serializer.cs
@@ -1,38 +1,41 @@
-﻿using System.IO;
+﻿using MimeKit;
+using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
 
 namespace SaintSender.Control
 {
     internal class Serializer : ISerializer
     {
-        public Mail[] Restore(string folderName)
+        public MimeMessage[] Restore(string folderName)
         {
-            // folderName should be a full path
-            string[] filePaths = Directory.GetFiles(Path.GetDirectoryName(folderName));
+            //// folderName should be a full path
+            //string[] filePaths = Directory.GetFiles(Path.GetDirectoryName(folderName));
 
-            Mail[] mails = new Mail[filePaths.Length];
-            BinaryFormatter bf = new BinaryFormatter();
+            //MimeMessage[] mails = new MimeMessage[filePaths.Length];
+            //BinaryFormatter bf = new BinaryFormatter();
 
-            for (int i = 0; i < mails.Length; i++)
-            {
-                string fileName = filePaths[i];
-                FileStream stream = new FileStream(fileName, FileMode.Open);
-                mails[i] = (Mail)bf.Deserialize(stream);
-                stream.Close();
-            }
-            return mails;
+            //for (int i = 0; i < mails.Length; i++)
+            //{
+            //    string fileName = filePaths[i];
+            //    FileStream stream = new FileStream(fileName, FileMode.Open);
+            //    mails[i] = (MimeMessage)bf.Deserialize(stream);
+            //    stream.Close();
+            //}
+            return null;
         }
 
-        public void Save(Mail[] mails, string folderName)
+        public void Save(MimeMessage[] mails, string folderName)
         {
             BinaryFormatter bf = new BinaryFormatter();
 
+            MessageConverter m = new MessageConverter();
+
             for (int i = 0; i < mails.Length; i++)
             {
-                // TODO: name of the file to serialize - email ID ??
-                string fileName = Path.GetDirectoryName(folderName) + "BLABLA.dat";
+                // TODO: check whether MessageId is exist or not
+                string fileName = Path.GetDirectoryName(folderName) + @"\" + mails[i].MessageId + ".dat";
                 FileStream stream = new FileStream(fileName, FileMode.Create);
-                bf.Serialize(stream, mails[i]);
+                bf.Serialize(stream, m.ToMail(mails[i]));
                 stream.Close();
             }
         }
