ac7bacb7a792e3b467c107ef80dbbc0083f44db6
Imre Meszesan <imre.meszesan@gmail.com>
2017-07-18 14:43:24 +0200

    created skeelton SaintSender class

---
+++ SaintSender/SaintSender/Control/SaintSender.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MimeKit;
+using SaintSender.Model;
+
+namespace SaintSender.Control
+{
+    class SaintSender : IClient
+    {
+        public IClient INSTANCE { get; }
+
+        private SaintSender()
+        {
+
+        }
+
+        public void Initialize()
+        {
+            throw new NotImplementedException();
+        }
+
+        public MimeMessage[] DownloadMails()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Connect(ConnectionInfo imapInfo, ConnectionInfo smtpInfo)
+        {
+            // IConnection conn = new IConnectionImpl(imapInfo, smtpInfo);
+            // conn.Connect();
+        }
+
+        public void AutoRefresh()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SendMail()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void BackupMails()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RestoreMails()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Login()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
