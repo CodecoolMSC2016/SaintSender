95419d4e55cd91f75ba393f0eff80baf41339a5f
bozsob <bozso.beatrix@gmail.com>
2017-07-20 09:57:39 +0200

    added a comment

--- SaintSender/SaintSender/Control/Searcher.cs
+++ SaintSender/SaintSender/Control/Searcher.cs
@@ -6,6 +6,13 @@ namespace SaintSender.Control
 {
     internal class Searcher
     {
+        //private string emailPattern = @"^(?("")("".+?(?<!\\)""@) |
+        //(([0-9a-z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-z])@))" +
+        //                 @"(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\]) |
+        //(([0-9a-z][-\w]*[0-9a-z]*\.)+[a-z0-9][\-a-z0-9]{0,22}[a-z0-9]))$";
+        // validate the Email
+        //if (Regex.IsMatch(txtEmail.Text, emailPattern, RegexOptions.IgnoreCase))
+
         public void SearchByMailboxAddress(string pattern)
         {
             List<InternetAddress> result = new List<InternetAddress>();
