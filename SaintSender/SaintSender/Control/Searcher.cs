a73924cc02091ebb9d58d8da8e9d70f668fd3868
bozsob <bozso.beatrix@gmail.com>
2017-07-20 08:52:16 +0200

    implementation of methods SearchByMailboxAddress() SearchByMailboyName()

--- SaintSender/SaintSender/Control/Searcher.cs
+++ SaintSender/SaintSender/Control/Searcher.cs
@@ -1,50 +1,44 @@
 ﻿using MimeKit;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
-
-//An InternetAddressList is a list of InternetAddress items.
-//An InternetAddress is an abstract class with 2 subclasses: GroupAddress and MailboxAddress.
-
-//A MailboxAddress is what you probably consider an "email address" that 
-//    comes in the form "John Smith" <john @smith-plumbing.com>.
-//The Name property on the MailboxAddress contains the string John Smith 
-//    while the Address property contains the string john@smith-plumbing.com.
-
-//A GroupAddress is something that is rarely used these days.
-//It is a named list of addresses that might look something like this 
-//    if you ever find an example of one 
-//    in a message's To or Cc header: Friends: Joey <joey@friends.com>, Monica <monica@friends.com>, 
-//    "Mrs. Chanandler Bong" <chandler@friends.com>, Ross <ross@firnds.com>, Rachel <rachel@friends.com>;
-
-//Since you probably only care about the individual email addresses on the From header of the message, 
-//    you could just use this:
-
-//foreach (var mailbox in message.From.Mailboxes)
-//    Console.WriteLine("{0}'s email address is {1}", mailbox.Name, mailbox.Address);
-
 
 namespace SaintSender.Control
 {
-    class Searcher
+    internal class Searcher
     {
-        
-        
-
-        public string SearchByMailboxAddress(string pattern)
+        public void SearchByMailboxAddress(string pattern)
         {
+            List<InternetAddress> result = new List<InternetAddress>();
 
-            return null;
+            Regex regex = new Regex(pattern, RegexOptions.IgnoreCase);
+            MatchCollection matches = regex.Matches("....");
+
+            // needed the Form property of MimeMessage
+            foreach (MailboxAddress mailbox in message.From.Mailboxes)
+            {
+                if (Regex.IsMatch(mailbox.Address, pattern))
+                {
+                    result.Add(mailbox);
+                }
+            }
         }
 
-        public string SearchByName(string pattern)
+        public List<InternetAddress> SearchByName(string pattern)
         {
+            List<InternetAddress> result = new List<InternetAddress>();
 
+            Regex regex = new Regex(pattern, RegexOptions.IgnoreCase);
+            MatchCollection matches = regex.Matches("....");
 
-            return null;
+            // needed the Form property of MimeMessage
+            foreach (MailboxAddress mailbox in message.From.Mailboxes)
+            {
+                if (Regex.IsMatch(mailbox.Name, pattern))
+                {
+                    result.Add(mailbox);
+                }
+            }
+            return result;
         }
     }
 }
