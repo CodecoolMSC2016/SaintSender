063e79e8dffc0bb8c7321513f0ecec22c5fcf8be
bozsob <bozso.beatrix@gmail.com>
2017-07-21 10:42:54 +0200

    using SaintClient instance to get Mails

--- SaintSender/SaintSender/Control/Searcher.cs
+++ SaintSender/SaintSender/Control/Searcher.cs
@@ -1,4 +1,6 @@
-﻿using MimeKit;
+﻿using MailKit.Net.Imap;
+using MimeKit;
+using SaintSender.Model;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
 
@@ -6,14 +8,22 @@ namespace SaintSender.Control
 {
     internal class Searcher
     {
-        private IReceiver receiver;
+        
+        private MessageReceiver receiver;
+        private SaintClient client;
+        //private SaintClient client = SaintClient.INSTANCE;
 
         public MimeMessage[] SearchMessage(string pattern)
         {
             HashSet<MimeMessage> searchResultSet = new HashSet<MimeMessage>();
 
-            MimeMessage[] messages = receiver.Mails;
+            // MimeMessage[] messages = receiver.Mails;
 
+            ConnectionInfo imapInfo = new ConnectionInfo("imap.gmail.com", 993);
+            ConnectionInfo smtpInfo = new ConnectionInfo("smtp.gmail.com", 465);
+            client = new SaintClient(imapInfo, smtpInfo, "imaptest420@gmail.com", "024tsetpami");
+
+            MimeMessage[] messages = client.DownloadMails();
             for (int i = 0; i < messages.Length; i++)
             {
                 if (Regex.IsMatch(messages[i].Subject, pattern, RegexOptions.IgnoreCase))
